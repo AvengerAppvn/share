525eb01f7a6df564ab7c9a882f532eead87a4c01
Nguyen Duc Tho <thondh3d@house3d.net>
2018-04-07 10:44:07 +0700

    add api

---
+++ frontend/modules/api/v1/controllers/NotificationController.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace frontend\modules\api\v1\controllers;
+
+use frontend\models\UserEditForm;
+use backend\models\LoginForm;
+use common\models\User;
+use frontend\modules\api\v1\resources\User as UserResource;
+use frontend\modules\user\models\SignupConfirmForm;
+use frontend\modules\user\models\SignupForm;
+use yii\filters\AccessControl;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpBearerAuth;
+use yii\helpers\Url;
+use yii\rest\ActiveController;
+use yii\web\HttpException;
+use yii\web\NotFoundHttpException;
+
+/**
+ * @author Eugene Terentev <eugene@terentev.net>
+ */
+class NotificationController extends ActiveController
+{
+    /**
+     * @var string
+     */
+    public $modelClass = 'frontend\modules\api\v1\resources\Notification';
+
+    public function __construct($id, $module, $config = [])
+    {
+        parent::__construct($id, $module, $config);
+
+    }
+
+    public function actions()
+    {
+        return [];
+    }
+
+    /**
+     * @return array
+     */
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        $behaviors['authenticator'] = [
+            'class' => CompositeAuth::className(),
+            'authMethods' => [
+                HttpBearerAuth::className(),
+            ],
+
+        ];
+
+        $behaviors['verbs'] = [
+            'class' => \yii\filters\VerbFilter::className(),
+            'actions' => [
+                'index' => ['get'],
+                'view' => ['get'],
+            ],
+        ];
+
+        // remove authentication filter
+        $auth = $behaviors['authenticator'];
+        unset($behaviors['authenticator']);
+
+        // add CORS filter
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::className(),
+            'cors' => [
+                'Origin' => ['*'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+                'Access-Control-Request-Headers' => ['*'],
+            ],
+        ];
+
+        // re-add authentication filter
+        $behaviors['authenticator'] = $auth;
+        // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
+        $behaviors['authenticator']['except'] = ['options', 'login', 'signup', 'confirm', 'password-reset-request', 'password-reset-token-verification', 'password-reset'];
+
+
+        // setup access
+        $behaviors['access'] = [
+            'class' => AccessControl::className(),
+            'only' => ['index', 'view', 'create', 'update', 'delete', 'me'], //only be applied to
+            'rules' => [
+                [
+                    'allow' => true,
+                    'actions' => ['index', 'view'],
+                    'roles' => ['admin', 'manageUsers'],
+                ],
+                [
+                    'allow' => true,
+                    'actions' => ['index', 'view'],
+                    'roles' => ['user']
+                ]
+            ],
+        ];
+
+        return $behaviors;
+    }
+
+    public function actionIndex()
+    {
+        $page_size = Yii::$app->request->get('page_size');
+        $page_index = Yii::$app->request->get('page_index');
+        if (!$page_size) {
+            $page_size = 8;
+        }
+
+        if (!$page_index) {
+            $page_index = 1;
+        }
+
+        $index = $page_size * ($page_index - 1);
+
+        $response = \Yii::$app->getResponse();
+        $response->setStatusCode(200);
+
+        $categories = AdsCategory::find()->limit($page_size)->offset($index)->all();
+        $categoriesResult = [];
+
+        foreach ($categories as $category) {
+
+            $categoriesResult[] = array(
+                'id' => $category->id,
+                'name' => $category->name,
+                'thumbnail' => $category->thumbnail,
+                'new' => 0,
+
+            );
+        }
+        return $categoriesResult;
+    }
+    public function actionView()
+    {
+        $response = \Yii::$app->getResponse();
+        // ads_id
+        $ads_id = Yii::$app->request->get('ads_id');
+        if(!$ads_id){
+            $response->setStatusCode(422);
+            return array(
+                'name'=> 'Thiếu tham số',
+                'message'=> array('ads_id'=> 'Thiếu tham số ads_id'),
+                'code'=> 0,
+                'status'=> 422,
+            );
+        }
+
+
+        $advertise = Advertise::findOne($ads_id);
+        if(!$advertise){
+            $response->setStatusCode(404);
+            return array(
+                'name'=> 'Không có dữ liệu',
+                'message'=> array('ads_id'=> 'Không tìm dược dữ liệu với id='.$ads_id),
+                'code'=> 0,
+                'status'=> 404,
+            );
+        }
+
+        $response->setStatusCode(200);
+        return array(
+            'id' => $advertise->id,
+            'title' => $advertise->title,
+            'description' => $advertise->description,
+            'content' => $advertise->content,
+            'thumbnail' => $advertise->thumb,
+            //'images' => $advertise->advertiseImages,
+            'images' => [$advertise->thumb],
+            'created_at' => date('Y-m-d H:i:s',$advertise->created_at),
+            'share' => $advertise->share ?: 0,
+        );
+    }
+
+    public function actionOptions($id = null)
+    {
+        return "ok";
+    }
+
+    /**
+     * @param $id
+     * @return null|static
+     * @throws NotFoundHttpException
+     */
+    public function findModel($id)
+    {
+        $model = UserResource::findOne($id);
+        if (!$model) {
+            throw new NotFoundHttpException;
+        }
+        return $model;
+    }
+}
---
+++ frontend/modules/api/v1/controllers/WalletController.php
@@ -0,0 +1,378 @@
+<?php
+
+namespace frontend\modules\api\v1\controllers;
+
+use frontend\models\UserEditForm;
+use backend\models\LoginForm;
+use common\models\User;
+use frontend\modules\api\v1\resources\User as UserResource;
+use frontend\modules\user\models\SignupConfirmForm;
+use frontend\modules\user\models\SignupForm;
+use yii\filters\AccessControl;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpBearerAuth;
+use yii\helpers\Url;
+use yii\rest\ActiveController;
+use yii\web\HttpException;
+use yii\web\NotFoundHttpException;
+
+/**
+ * @author Eugene Terentev <eugene@terentev.net>
+ */
+class WalletController extends ActiveController
+{
+    /**
+     * @var string
+     */
+    public $modelClass = 'frontend\modules\api\v1\resources\User';
+
+    public function __construct($id, $module, $config = [])
+    {
+        parent::__construct($id, $module, $config);
+
+    }
+
+    public function actions()
+    {
+        return [];
+    }
+
+    /**
+     * @return array
+     */
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        $behaviors['authenticator'] = [
+            'class' => CompositeAuth::className(),
+            'authMethods' => [
+                HttpBearerAuth::className(),
+            ],
+
+        ];
+
+        $behaviors['verbs'] = [
+            'class' => \yii\filters\VerbFilter::className(),
+            'actions' => [
+                'index' => ['get'],
+                'view' => ['get'],
+                'create' => ['post'],
+                'update' => ['put'],
+                'delete' => ['delete'],
+                'login' => ['post'],
+                'me' => ['get', 'post'],
+                'profile' => ['get', 'post'],
+                'emotion' => ['get'],
+                'addresses' => ['get'],
+            ],
+        ];
+
+        // remove authentication filter
+        $auth = $behaviors['authenticator'];
+        unset($behaviors['authenticator']);
+
+        // add CORS filter
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::className(),
+            'cors' => [
+                'Origin' => ['*'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+                'Access-Control-Request-Headers' => ['*'],
+            ],
+        ];
+
+        // re-add authentication filter
+        $behaviors['authenticator'] = $auth;
+        // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
+        $behaviors['authenticator']['except'] = ['options', 'login', 'signup', 'confirm', 'password-reset-request', 'password-reset-token-verification', 'password-reset'];
+
+
+        // setup access
+        $behaviors['access'] = [
+            'class' => AccessControl::className(),
+            'only' => ['index', 'view', 'create', 'update', 'delete', 'me'], //only be applied to
+            'rules' => [
+                [
+                    'allow' => true,
+                    'actions' => ['index', 'view', 'create', 'update', 'delete'],
+                    'roles' => ['admin', 'manageUsers'],
+                ],
+                [
+                    'allow' => true,
+                    'actions' => ['me','update'],
+                    'roles' => ['user']
+                ]
+            ],
+        ];
+
+        return $behaviors;
+    }
+
+//    public function actionCreate()
+//    {
+//        $model = new User();
+//        $model->load(\Yii::$app->getRequest()->getBodyParams(), '');
+//
+//        if ($model->validate() && $model->save()) {
+//            $response = \Yii::$app->getResponse();
+//            $response->setStatusCode(201);
+//            $id = implode(',', array_values($model->getPrimaryKey(true)));
+//            $response->getHeaders()->set('Location', Url::toRoute([$id], true));
+//        } else {
+//            // Validation error
+//            throw new HttpException(422, json_encode($model->errors));
+//        }
+//
+//        return $model;
+//    }
+
+    public function actionLogin()
+    {
+        $model = new LoginForm();
+//        $model->load(\Yii::$app->request->post(), '');
+//        $response = \Yii::$app->getResponse();
+//        $response->setStatusCode(200);
+//        return $model;
+        if ($model->load(\Yii::$app->request->post(), '') && $model->login()) {
+            $user = $model->getUser();
+            $user->generateAccessTokenAfterUpdatingClientInfo(true);
+
+            $response = \Yii::$app->getResponse();
+            $response->setStatusCode(200);
+            $id = implode(',', array_values($user->getPrimaryKey(true)));
+
+            $responseData = [
+                'id' => (int)$id,
+                'access_token' => $user->access_token,
+            ];
+
+            return $responseData;
+        } else {
+            // Validation error
+            throw new HttpException(422, json_encode($model->errors));
+        }
+    }
+
+    public function actionSignup()
+    {
+        $model = new SignupForm();
+
+        $model->load(\Yii::$app->request->post(), '');
+        $model->username = $model->email;
+
+        if ($model->validate() && ($result = $model->signup())) {
+            // Send confirmation email
+            $model->sendConfirmationEmail();
+
+            $response = \Yii::$app->getResponse();
+            $response->setStatusCode(200);
+
+            $user = User::findOne($result);
+            $user->generateAccessTokenAfterUpdatingClientInfo(true);
+
+            $responseData = array(
+                'id' => $user->id,
+                'access_token' => $user->access_token,
+                'email' => $user->email,
+                'created_at' => date('Y-m-d H:i:s', $user->created_at),
+                //'status' => $user->status,
+            );
+
+            return $responseData;
+        } else {
+            // Validation error
+            throw new HttpException(422, json_encode($model->errors));
+        }
+    }
+
+    public function actionConfirm()
+    {
+        $model = new SignupConfirmForm();
+
+        $model->load(\Yii::$app->request->post());
+        if ($model->validate() && $model->confirm()) {
+
+            $response = \Yii::$app->getResponse();
+            $response->setStatusCode(200);
+
+            $user = $model->getUser();
+            $responseData = [
+                'id' => (int)$user->id,
+                'access_token' => $user->access_token,
+            ];
+
+            return $responseData;
+
+        } else {
+            // Validation error
+            throw new HttpException(422, json_encode($model->errors));
+        }
+    }
+
+    public function actionPasswordResetRequest()
+    {
+        $model = new PasswordResetRequestForm();
+
+        $model->load(Yii::$app->request->post());
+        if ($model->validate() && $model->sendPasswordResetEmail()) {
+
+            $response = \Yii::$app->getResponse();
+            $response->setStatusCode(200);
+
+            $responseData = "true";
+
+            return $responseData;
+        } else {
+            // Validation error
+            throw new HttpException(422, json_encode($model->errors));
+        }
+    }
+
+    public function actionPasswordResetTokenVerification()
+    {
+        $model = new PasswordResetTokenVerificationForm();
+
+        $model->load(Yii::$app->request->post());
+        if ($model->validate() && $model->validate()) {
+
+            $response = \Yii::$app->getResponse();
+            $response->setStatusCode(200);
+
+            $responseData = "true";
+
+            return $responseData;
+        } else {
+            // Validation error
+            throw new HttpException(422, json_encode($model->errors));
+        }
+    }
+
+    /**
+     * Resets password.
+     */
+    public function actionPasswordReset()
+    {
+        $model = new PasswordResetForm();
+        $model->load(Yii::$app->request->post());
+
+        if ($model->validate() && $model->resetPassword()) {
+
+            $response = \Yii::$app->getResponse();
+            $response->setStatusCode(200);
+
+            $responseData = "true";
+
+            return $responseData;
+        } else {
+            // Validation error
+            throw new HttpException(422, json_encode($model->errors));
+        }
+    }
+
+    /**
+     * Rest Description: Your endpoint description.
+     * Rest Fields: ['field1', 'field2'].
+     * Rest Filters: ['filter1', 'filter2'].
+     * Rest Expand: ['expandRelation1', 'expandRelation2'].
+     */
+    public function actionMe()
+    {
+        $user = User::findIdentity(\Yii::$app->user->getId());
+
+        if ($user) {
+            $response = \Yii::$app->getResponse();
+            $response->setStatusCode(200);
+            // TODO get from profile
+            $strengths = ['Thời trang', 'Điện tử', 'Du lịch'];
+            $coin = 0;
+            return array(
+                'fullname' => $user->userProfile->fullname,
+                'address' => $user->userProfile->address ?: '',
+                'email' => $user->email,
+                'phone' => $user->phone,
+                'avatar' => $user->userProfile->avatar ?: '',
+                'is_confirmed' => $user->is_confirmed ?: false,
+                'birthday' => $user->userProfile->birthday ?: '',
+                'strengths' => $strengths,
+                'coin' => $coin,
+                'is_customer' => $user->is_customer? true : false,
+                'is_advertiser' => $user->is_advertiser? true : false,
+                'status_confirmed' => $user->status_confirmed,
+            );
+        } else {
+            // Validation error
+            throw new NotFoundHttpException("Object not found");
+        }
+    }
+
+    public function actionUpdate()
+    {
+        $user = User::findIdentity(\Yii::$app->user->getId());
+
+        if ($user) {
+
+            $model = new UserEditForm();
+            $model->load(\Yii::$app->request->post(), '');
+            $model->id = $user->id;
+
+            if ($model->validate() && $model->save()) {
+                $response = \Yii::$app->getResponse();
+                $response->setStatusCode(200);
+                $user = $model->getUserByID();
+                return [
+                    'fullname' => $user->userProfile->fullname,
+                    'address' => $user->userProfile->address,
+                    'email' => $user->email,
+                    'phone' => $user->phone,
+                    //'avatar' => $user->userProfile->avatar,
+                    'birthday' => $user->userProfile->birthday,
+                    'strengths' => json_decode($user->userProfile->strengths),
+                    //'last_login_at' =>  $user->last_login_at,
+                    //'last_login_ip' =>  $user->last_login_ip,
+                ];
+            } else {
+                // Validation error
+                throw new HttpException(422, json_encode($model->errors));
+            }
+        } else {
+            // Validation error
+            throw new NotFoundHttpException("Object not found");
+        }
+    }
+
+    public function actionLogout()
+    {
+        $user = User::findIdentity(\Yii::$app->user->getId());
+
+        if ($user) {
+            $user->destroyAccessToken();
+        }
+
+        $response = \Yii::$app->getResponse();
+        $response->setStatusCode(200);
+
+        $responseData = "true";
+
+        return $responseData;
+    }
+
+    public function actionOptions($id = null)
+    {
+        return "ok";
+    }
+
+    /**
+     * @param $id
+     * @return null|static
+     * @throws NotFoundHttpException
+     */
+    public function findModel($id)
+    {
+        $model = UserResource::findOne($id);
+        if (!$model) {
+            throw new NotFoundHttpException;
+        }
+        return $model;
+    }
+}
