5ea6eeea883adb4da29c4fed5a43a6c1d3272e91
Nguyen Duc Tho <thondh3d@house3d.net>
2018-04-07 22:12:22 +0700

    add notification

--- frontend/modules/api/v1/controllers/HomeController.php
+++ frontend/modules/api/v1/controllers/HomeController.php
@@ -171,13 +171,21 @@ class HomeController extends ActiveController
         $advertisesResult = [];
 
         foreach ($advertises as $advertise) {
-
+            $user = User::findOne($advertise->created_by);
+            $customer_avatar = null;
+            $customer_name = null;
+            if($user){
+                $customer_avatar = $user->userProfile->avatar;
+                $customer_name = $user->userProfile->fullname;
+            }
             $advertisesResult[] = array(
                 'id' => $advertise->id,
                 'title' => $advertise->title,
                 'description' => $advertise->description,
                 'thumbnail' => $advertise->thumb,
                 'created_at' => date('Y-m-d H:i:s',$advertise->created_at),
+                'customer_avatar' => $customer_avatar?:'',
+                'customer_name' => $customer_name?:'',
             );
         }
         return $advertisesResult;
