4505eda265eed02a7252a19b5b76ac60e956d4ac
Nguyen Duc Tho <thondh3d@house3d.net>
2018-04-09 18:41:23 +0700

    api list bank

--- frontend/modules/api/v1/controllers/BankController.php
+++ frontend/modules/api/v1/controllers/BankController.php
@@ -103,8 +103,8 @@ class BankController extends ActiveController
 
     public function actionIndex()
     {
-        $page_size = Yii::$app->request->get('page_size');
-        $page_index = Yii::$app->request->get('page_index');
+        $page_size = \Yii::$app->request->get('page_size');
+        $page_index = \Yii::$app->request->get('page_index');
         if (!$page_size) {
             $page_size = 8;
         }
