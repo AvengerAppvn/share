0be0f85fffc480f5d3319464c507b6066fcd6cda
Nguyen Duc Tho <thondh3d@house3d.net>
2018-04-28 14:24:01 +0700

    home category home

--- frontend/mail/passwordResetToken.php
+++ frontend/mail/passwordResetToken.php
@@ -8,8 +8,18 @@ use yii\helpers\Html;
 $resetLink = Yii::$app->urlManager->createAbsoluteUrl(['/user/sign-in/reset-password', 'token' => $token]);
 ?>
 
-Hello <?php echo Html::encode($user->username) ?>,
-
-Follow the link below to reset your password:
-
 <?php echo Html::a(Html::encode($resetLink), $resetLink) ?>
+<div style="text-align: left; word-wrap: break-word;">Xin kính chào <b><?= Html::encode($user->username) ?></b>,<br />
+    <br />
+    Bạn hoặc ai đó xác nhận rằng bạn (Tài khoản: <b><?= Html::encode($user->username) ?></b>) quên mật khẩu tại Shareme.
+    <br />
+    . Nếu không phải từ bạn, xin vui lòng bỏ qua email này.
+    <br /><br />
+    . Nếu đúng là từ bạn, nhấn vào <a href="<?=Html::encode($resetLink);?>">đây</a> để tạo lại mật khẩu. Bạn cũng có thể copy/paste <?=$resetLink;?> vào trình duyệt web.
+    <br />
+    <br />Trân trọng,
+    <br />Hỗ trợ Shareme
+    <div class="footer" style="font-size: 0.7em; padding: 0px; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; text-align: right; color: #777777; line-height: 14px; margin-top: 36px;">© <?=date("Y");?> Shareme
+        <br>
+    </div>
+</div>
