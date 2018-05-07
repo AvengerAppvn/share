7175ec90d721f2eab77949dc4f26ba0b99920351
Nguyen Duc Tho <thondh3d@house3d.net>
2018-05-07 08:36:30 +0700

    home category home

--- frontend/modules/api/v1/controllers/UserController.php
+++ frontend/modules/api/v1/controllers/UserController.php
@@ -313,7 +313,7 @@ class UserController extends ActiveController
             $coin = 0;
             $wallet = Wallet::find()->where(['user_id' => $user->id])->one();
             if ($wallet) {
-                $coin = $wallet->amount;
+                $coin = (int)$wallet->amount;
             }
             return array(
                 'fullname' => $user->userProfile->fullname,
