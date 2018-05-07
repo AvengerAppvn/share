53e3bc507f7c3f91161c45c81caf4f645ffecc6a
Nguyen Duc Tho <thondh3d@house3d.net>
2018-05-07 15:46:44 +0700

    revert

--- backend/views/user/index.php
+++ backend/views/user/index.php
@@ -2,7 +2,7 @@
 
 use yii\grid\GridView;
 use yii\helpers\Html;
-use kartik\export\ExportMenu;
+
 /* @var $this yii\web\View */
 /* @var $searchModel backend\models\search\UserSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -18,65 +18,55 @@ $this->params['breadcrumbs'][] = $this->title;
         ]), ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
-    <?php
-    $gridColumns = [
-        [
-            'attribute' => 'id',
-            'headerOptions' => ['style' => 'width:50px'],
+    <?php echo GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'options' => [
+            'class' => 'grid-view table-responsive'
         ],
-        'username',
-        'email:email',
-        'phone',
-        [
-            'attribute' => 'is_customer',
-            'format' => 'html',
-            'value' => function ($model) {
-                return $model->is_customer ? Html::tag("i", '', ['class' => 'fa fa-check text-success']) : Html::tag("i", '', ['class' => 'fa fa-close text-danger']);
-            },
-        ],
-        [
-            'attribute' => 'is_advertiser',
-            'label' => 'Nhà QC',
-            'format' => 'html',
-            'value' => function ($model) {
-                return $model->is_advertiser ? Html::tag("i", '', ['class' => 'fa fa-check text-success']) : Html::tag("i", '', ['class' => 'fa fa-close text-danger']);
-            },
-        ],
-        'created_at:datetime',
-        'logged_at:datetime',
-        // 'updated_at',
+        'columns' => [
+            [
+                'attribute' => 'id',
+                'headerOptions' => ['style' => 'width:50px'],
+            ],
+            'username',
+            'email:email',
+            'phone',
+            [
+                'attribute' => 'is_customer',
+                'format' => 'html',
+                'value' => function ($model) {
+                    return $model->is_customer ? Html::tag("i", '', ['class' => 'fa fa-check text-success']) : Html::tag("i", '', ['class' => 'fa fa-close text-danger']);
+                },
+            ],
+            [
+                'attribute' => 'is_advertiser',
+                'label' => 'Nhà QC',
+                'format' => 'html',
+                'value' => function ($model) {
+                    return $model->is_advertiser ? Html::tag("i", '', ['class' => 'fa fa-check text-success']) : Html::tag("i", '', ['class' => 'fa fa-close text-danger']);
+                },
+            ],
+            'created_at:datetime',
+            'logged_at:datetime',
+            // 'updated_at',
 
-        [
-            'label' => 'Trạng thái',
-            'format' => 'html',
-            'attribute' => 'is_confirmed',
-            'value' => function ($model) {
-                return $model->is_confirmed == 1 ?
-                    Html::tag("i", ' Đã xác thực', ['class' => 'fa fa-check text-success']):
-                    Html::a("Kiểm tra", ['confirm', 'id' => $model->id], ['target' => '_blank', 'class' => 'btn btn-success']);
-            },
-        ],
-        [
-            'class' => 'yii\grid\ActionColumn',
-            'options' => [
-                'style' => 'width:80px;',
+            [
+                'label' => 'Trạng thái',
+                'format' => 'html',
+                'value' => function ($model) {
+                    return $model->is_confirmed == 1 ?
+                        Html::tag("i", ' Đã xác thực', ['class' => 'fa fa-check text-success']):
+                        Html::a("Kiểm tra", ['confirm', 'id' => $model->id], ['target' => '_blank', 'class' => 'btn btn-success']);
+                },
+            ],
+            [
+                'class' => 'yii\grid\ActionColumn',
+                'options' => [
+                    'style' => 'width:80px;',
+                ]
             ]
-        ]
-    ];
-
-    // Renders a export dropdown menu
-//    echo ExportMenu::widget([
-//        'dataProvider' => $dataProvider,
-//        'columns' => $gridColumns
-//    ]);
-//
-//    echo \kartik\grid\GridView::widget([
-//        'dataProvider' => $dataProvider,
-//        'filterModel' => $searchModel,
-//        'options' => [
-//            'class' => 'grid-view table-responsive'
-//        ],
-//        'columns' => $gridColumns
-//    ]); ?>
+        ],
+    ]); ?>
 
 </div>
