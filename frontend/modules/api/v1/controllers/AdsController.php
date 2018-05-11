4deb5dd9c2917a0ae82b8a41e738d952ffdd053c
Nguyen Duc Tho <thondh3d@house3d.net>
2018-05-11 22:23:44 +0700

    home category home

--- frontend/modules/api/v1/controllers/AdsController.php
+++ frontend/modules/api/v1/controllers/AdsController.php
@@ -295,7 +295,14 @@ class AdsController extends ActiveController
             }else{
                 // configure with favored image driver (gd by default)
                 Image::configure(array('driver' => 'imagick'));
-                $image = Image::make('img/bg_color.png')->text($advertise->description,100,200);
+                $image = Image::make('img/bg_color.png')->text($advertise->description,100,200,function($font) {
+                    $font->file('font/arial.ttf');
+                    $font->size(24);
+                    $font->color('#fdf6e3');
+                    $font->align('center');
+                    $font->valign('top');
+                    $font->angle(45);
+                });
                 //
                 $image->save(\Yii::getAlias('@storage') . '/web/source/shares/bg_color_'.$advertise->id.'.png');
 
