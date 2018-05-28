dea20b0e2b2ee87aa2128e26fe23fc2606d14f3c
Nguyen Duc Tho <thondh3d@house3d.net>
2018-05-28 09:14:13 +0700

    hide notification

--- frontend/models/AdsForm.php
+++ frontend/models/AdsForm.php
@@ -149,14 +149,14 @@ class AdsForm extends Model
 
                 }
                 // 3 Khi có ads phù hợp
-                $message = array('en'=>'Bạn có quảng cáo phù hợp với chuyên môn của bạn');
-                //$options = array( "include_player_ids"=> ["a7d48d0a-fa11-4cf8-a412-fc3f56388ad2"],"data"=> array("ads_id"=> $model->id),);
-                $filters = array();
-                foreach ($tags as $tag){
-                    $filters[] = array("field" => "tag", "key" => $tag, "relation" => "=", "value" => "1");
-                }
-                $options = array( 'filters' => $filters,"data"=> array("type"=>3,"ads_id"=> $model->id),"buttons"=>[array("id"=> "1", "text"=> "View","icon"=>"")]);
-                \Yii::$app->onesignal->notifications()->create($message, $options);
+//                $message = array('en'=>'Bạn có quảng cáo phù hợp với chuyên môn của bạn');
+//                //$options = array( "include_player_ids"=> ["a7d48d0a-fa11-4cf8-a412-fc3f56388ad2"],"data"=> array("ads_id"=> $model->id),);
+//                $filters = array();
+//                foreach ($tags as $tag){
+//                    $filters[] = array("field" => "tag", "key" => $tag, "relation" => "=", "value" => "1");
+//                }
+//                $options = array( 'filters' => $filters,"data"=> array("type"=>3,"ads_id"=> $model->id),"buttons"=>[array("id"=> "1", "text"=> "View","icon"=>"")]);
+//                \Yii::$app->onesignal->notifications()->create($message, $options);
 
                 return $model;
             } else {
