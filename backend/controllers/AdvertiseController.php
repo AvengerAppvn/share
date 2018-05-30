fa13ac01e42139fb9b7017ed4e1f71b8429b64ee
Nguyen Duc Tho <thondh3d@house3d.net>
2018-05-30 23:40:19 +0700

    dashboard

--- backend/controllers/AdvertiseController.php
+++ backend/controllers/AdvertiseController.php
@@ -110,10 +110,26 @@ class AdvertiseController extends Controller
                     if ($device && $device->player_id) {
                         $message = array('en' => 'Chúc mừng! Quảng cáo của bạn đã được phê duyệt.');
                         $options = array("include_player_ids" => [$device->player_id],
-                            "data" => array('type' => 5, 'ads_id' => $model->id, 'push_id' => 0, 'post_id' => ''),"buttons"=>[array("id"=> "1", "text"=> "View","icon"=>"")]);
+                            "data" => array('type' => 5, 'ads_id' => $model->id, 'push_id' => 0, 'post_id' => ''), "buttons" => [array("id" => "1", "text" => "View", "icon" => "")]);
 
                         \Yii::$app->onesignal->notifications()->create($message, $options);
                     }
+
+                    $tags = [];
+
+                    $categories = CategoryAds::findAll(['ads_id'=>$id]);
+                    foreach ($categories as $category) {
+                        $tags[]= $category->slug;
+                    }
+
+                    $message = array('en' => 'Bạn có quảng cáo phù hợp với chuyên môn của bạn');
+                    //$options = array( "include_player_ids"=> ["a7d48d0a-fa11-4cf8-a412-fc3f56388ad2"],"data"=> array("ads_id"=> $model->id),);
+                    $filters = array();
+                    foreach ($tags as $tag) {
+                        $filters[] = array("field" => "tag", "key" => $tag, "relation" => "=", "value" => "1");
+                    }
+                    $options = array('filters' => $filters, "data" => array("type" => 3, "ads_id" => $model->id), "buttons" => [array("id" => "1", "text" => "View", "icon" => "")]);
+                    \Yii::$app->onesignal->notifications()->create($message, $options);
                 }
                 return $this->redirect(['view', 'id' => (string)$model->id]);
             } else {
